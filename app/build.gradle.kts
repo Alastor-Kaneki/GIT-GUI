1814742904f606d3668d86005130a4bd25605443
Alastor Kaneki <170276077+Alastor-Kaneki@users.noreply.github.com>
2026-08-01 14:49:42 -0400

    Bump Java diagnostic build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.alastorkaneki.gitgui"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.2.3"
+        versionCode = 5
+        versionName = "0.2.4"
         buildConfigField("String", "GITHUB_CLIENT_ID", "\"${githubClientId.get()}\"")
     }
 
